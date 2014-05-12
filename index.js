375af154dbb9013a010868a7ebcc1e682edb3446
zhangyaochun <zhangyaochun@wandoujia.com>
2014-05-12 14:08:27 +0800

    add file and other lib

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 'use strict';
 
-//version
-exports.version = function() {
-    
-}
+var yc = module.exports = {};
+
+//util
+yc.util = require('./lib/util.js');
+
+//file
+yc.file = require('./lib/file.js');
+
+//log
+yc.log = require('./lib/log.js');
+
+//config
+yc.config = require('./lib/config.js');
